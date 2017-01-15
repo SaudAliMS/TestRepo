3efe40a0b7d7ac2adbaf4b9b5f776a1c5699a43f
Saud <saud.ali@mindstormstudios.com>
2017-01-15 18:40:38 +0500

    project settings

---
+++ TestProject/Assets/Editor/BuildScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Linq;
+
+public class BuildScript
+{
+    static void PerformAndroidBuild()
+    {
+        BuildPipeline.BuildPlayer(GetScenes(), "android.apk", BuildTarget.Android, BuildOptions.None);
+    }
+
+    static string[] GetScenes()
+    {
+        return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+    }
+}
